fa66284d3bc95f9eb3347942bff39039e0539f2a
Edward Gaere <gaeree@student.ethz.ch>
2019-03-19 04:47:47 +0100

    removed Google BigQuery code that contained API keys

--- R/biquery/readme.txt
+++
@@ -1,4 +0,0 @@
-DIAS-GDELT/R/bigquery
-edward | 2018-02-19
-
-access GDELT data stored in Google BigQery directly from R
--- R/sql/tone.1country.timeline.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    *
-FROM 
-    gdelt
-WHERE 
-    ActionGeo_CountryCode = '<country>' 
-ORDER BY
-    globaleventid DESC
-LIMIT <num.rows>
